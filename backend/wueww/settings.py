cea9b32f5e1e2ac2ae5d7e9fd791f3cb898adbc3
Max Schmitt <max@schmitt.mx>
2020-03-27 16:30:16 +0100

    fix: cleanup from old gunicorn boilerplate (#2)
    * fix: cleanup
    
    * fix: it
    
    * fix: cleanup

--- backend/wueww/settings.py
+++ backend/wueww/settings.py
@@ -15,10 +15,6 @@ import environ
 
 env = environ.Env()
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -113,7 +109,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
@@ -123,5 +118,3 @@ ROOT_DIR = (
 
 STATIC_ROOT = str(ROOT_DIR("staticfiles"))
 STATIC_URL = "/static/"
-
-STATICFILES_DIRS = [os.path.join("..", "frontend", "build", "static")]
